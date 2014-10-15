a47fcd19e3b00b4b28f881b26f7316bae9b7f6ac
elena <elena.15@hotmail.it>
2014-10-15 14:49:36 +0100

    rock paper scissors in JS

---
+++ spec/rockPaperScissorsSpec.js
@@ -0,0 +1,61 @@
+describe('RPS', function(){
+
+		beforeEach(function(){
+			rock = new Rock
+			scissors = new Scissors
+			paper = new Paper
+		});
+
+	describe('Playing the game', function(){
+
+		beforeEach(function(){
+			game = new Game
+			draw = new Draw
+		});
+
+		it('Rock beats Scissors', function(){
+			expect(game.winner(rock, scissors)).toEqual(rock);
+		});
+
+		it('Scissors loses to rock', function(){
+			expect(game.winner(scissors, rock)).toEqual(rock);
+		});
+
+		it('Paper beats rock', function(){
+			expect(game.winner(paper, rock)).toEqual(paper);
+		});
+
+		it('Rock loses to paper', function(){
+			expect(game.winner(rock, paper)).toEqual(paper);
+		});
+
+		it('Scissor beats paper', function(){
+			expect(game.winner(scissors, paper)).toEqual(scissors);
+		});
+
+		it('Paper loses to scissors', function(){
+			expect(game.winner(paper, scissors)).toEqual(scissors);
+		});
+
+		it('Rock draws rock', function(){
+			expect(game.winner(rock, rock)).toEqual(draw);
+		});
+
+	});
+
+	describe('The types of weapons', function(){
+
+		it('rock', function(){
+			expect(rock.type).toEqual('Rock');
+		});
+
+		it('scissors', function(){
+			expect(scissors.type).toEqual('Scissors');
+		});
+
+		it('paper', function(){
+			expect(paper.type).toEqual('Paper');
+		});
+	});
+
+});
---
+++ src/rockPaperScissors.js
@@ -0,0 +1,23 @@
+function Rock(){
+	this.type = 'Rock'
+};
+function Scissors(){
+	this.type = 'Scissors'
+};
+function Paper(){
+	this.type = 'Paper'
+};
+
+function Game(){};
+function Draw(){};
+
+Game.prototype.winner= function(choice1, choice2) {
+	if(choice1.type === 'Rock' && choice2.type === 'Scissors') return choice1;
+	if(choice1.type === 'Paper' && choice2.type === 'Rock') return choice1;
+	if(choice1.type === 'Scissors' && choice2.type === 'Rock') return choice2;
+	if(choice1.type === 'Rock' && choice2.type === 'Paper') return choice2;
+	if(choice1.type === 'Scissors' && choice2.type === 'Paper') return choice1;
+	if(choice1.type === 'Paper' && choice2.type === 'Scissors') return choice2;
+	if(choice1.type === choice2.type) return draw;
+};
+
