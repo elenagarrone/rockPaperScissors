bea7356e3456a7f330cc880ee19f79b876dc7138
elena <elena.15@hotmail.it>
2014-10-20 19:14:25 +0100

    added html and css. still problems with the draw

--- src/rockPaperScissors.js
+++ src/rockPaperScissors.js
@@ -9,7 +9,9 @@ function Paper(){
 };
 
 function Game(){};
-function Draw(){};
+function Draw(){
+	this.type = "Draw"
+};
 
 Game.prototype.winner= function(choice1, choice2) {
 	if(choice1.type === 'Rock' && choice2.type === 'Scissors') return choice1;
@@ -18,6 +20,6 @@ Game.prototype.winner= function(choice1, choice2) {
 	if(choice1.type === 'Rock' && choice2.type === 'Paper') return choice2;
 	if(choice1.type === 'Scissors' && choice2.type === 'Paper') return choice1;
 	if(choice1.type === 'Paper' && choice2.type === 'Scissors') return choice2;
-	if(choice1.type === choice2.type) return draw;
+	if(choice1.type === choice2.type) return Draw.type;
 };
 
