e0ad7d9641b00abf3c3410f5194e62ebb9220c56
elena <elena.15@hotmail.it>
2014-10-21 18:05:33 +0100

    added public folder to clean up a bit

---
+++ public/game.js
@@ -0,0 +1,28 @@
+var options = {rock: new Rock('player'), paper: new Paper('player'), scissors: new Scissors('player')};
+var game = new Game
+var computer = function(){
+	choices = [new Rock('computer'), new Paper('computer'), new Scissors('computer')];
+	return choices[Math.floor(Math.random()* choices.length)];
+};
+var decideWinner = function(playerChoice){
+	var computerChoice = computer()
+	return game.winner(playerChoice, computerChoice)
+};
+
+var andTheWinnerIs = function(winner) {
+	if(winner.type !== undefined) {
+		$('#result').prepend(game.messageWin(winner));
+		$('#result').prepend('<li></li>')
+	} else {
+		$('#result').prepend('It was a draw');
+		$('#result').prepend('<li></li>')}
+		
+};
+
+var playerChoice = function(choice){
+	return options[$(choice).data('option')]
+}
+
+$('img').on('click', function(){
+	andTheWinnerIs(decideWinner(playerChoice(this)))
+})
---
+++ public/rockPaperScissors.js
@@ -0,0 +1,35 @@
+function Rock(owner){
+	this.type = 'Rock'
+	this.owner = owner;
+};
+function Scissors(owner){
+	this.type = 'Scissors'
+	this.owner = owner;
+};
+function Paper(owner){
+	this.type = 'Paper'
+	this.owner = owner;
+};
+
+function Game(){};
+
+
+Game.prototype.winner= function(choice1, choice2) {
+	if(choice1.type === 'Rock' && choice2.type === 'Scissors') return choice1;
+	if(choice1.type === 'Paper' && choice2.type === 'Rock') return choice1;
+	if(choice1.type === 'Scissors' && choice2.type === 'Rock') return choice2;
+	if(choice1.type === 'Rock' && choice2.type === 'Paper') return choice2;
+	if(choice1.type === 'Scissors' && choice2.type === 'Paper') return choice1;
+	if(choice1.type === 'Paper' && choice2.type === 'Scissors') return choice2;
+	if(choice1.type === choice2.type) return 'Draw';
+};
+
+Game.prototype.messageWin = function(winChoice) {
+	if(winChoice.owner === 'player') winnerName = 'You'
+	if(winChoice.owner === 'computer') winnerName = 'The computer'	
+	if(winChoice.type === 'Rock') winningAction = 'Rock destroys scissors';
+	if(winChoice.type === 'Scissors') winningAction = 'Scissors cuts paper';
+	if(winChoice.type === 'Paper') winningAction = 'Paper covers rock';
+	return winnerName + ' win! ' + winningAction
+};
+
