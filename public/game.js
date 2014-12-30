621c828bd9fe76679a912ded9c6b99fe24196253
Elena <elena.15@hotmail.it>
2014-12-30 11:02:14 +0000

    fixed indentation in public folder

--- public/game.js
+++ public/game.js
@@ -1,28 +1,29 @@
-var options = {rock: new Rock('player'), paper: new Paper('player'), scissors: new Scissors('player')};
 var game = new Game
+var options = { rock: new Rock('player'), paper: new Paper('player'), scissors: new Scissors('player') };
 var computer = function(){
-	choices = [new Rock('computer'), new Paper('computer'), new Scissors('computer')];
-	return choices[Math.floor(Math.random()* choices.length)];
+  choices = [new Rock('computer'), new Paper('computer'), new Scissors('computer')];
+  return choices[Math.floor(Math.random()* choices.length)];
 };
 var decideWinner = function(playerChoice){
-	var computerChoice = computer()
-	return game.winner(playerChoice, computerChoice)
+  var computerChoice = computer();
+  return game.winner(playerChoice, computerChoice);
 };
 
 var andTheWinnerIs = function(winner) {
-	if(winner.type !== undefined) {
-		$('#result').prepend(game.messageWin(winner));
-		$('#result').prepend('<li></li>')
-	} else {
-		$('#result').prepend('It was a draw');
-		$('#result').prepend('<li></li>')}
-		
+  if(winner.type !== undefined) {
+    $('#result').prepend(game.messageWin(winner));
+    $('#result').prepend('<li></li>');
+  } else {
+    $('#result').prepend('It was a draw');
+    $('#result').prepend('<li></li>');
+  };
+
 };
 
 var playerChoice = function(choice){
-	return options[$(choice).data('option')]
+  return options[$(choice).data('option')];
 }
 
 $('img').on('click', function(){
-	andTheWinnerIs(decideWinner(playerChoice(this)))
+  andTheWinnerIs(decideWinner(playerChoice(this)));
 })
